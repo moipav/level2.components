87101bbef4622f08884328aeee05149075261a65
--replace-all <moipav@ya.ru>
2022-07-21 11:39:35 +0800

    complete CRUD on DB component

--- classes/DB.php
+++ classes/DB.php
@@ -1,11 +1,14 @@
 <?php
+declare(strict_types=1);
 
 class DB
 {
     private static object|null $instance = null;
     private PDO $pdo;
-    private  $query, $results;
+    private PDOStatement|null $query;
+    private object|array $results;
     private bool $error = false;
+    private int $count;
 
     private function __construct()
     {
@@ -22,15 +25,31 @@ class DB
             self::$instance = new DB();
         }
         return self::$instance;
-    }
+    } 
 
-    public function query($sql)//:object|array
+    /**
+     * @param $sql
+     * @param array $params
+     * @return object|array
+     */
+    public function query($sql, array $params = []): object|array
     {
-        $this->query= $this->pdo->prepare($sql);
+        $this->error = false;
+        $this->query = $this->pdo->prepare($sql);
+
+        if (count($params)) {
+            $i = 1;
+            foreach ($params as $param) {
+                $this->query->bindValue($i, $param);
+                $i++;
+            }
+        }
         if (!$this->query->execute()) {
             $this->error = true;
+        } else {
+            $this->results = $this->query->fetchAll(PDO::FETCH_OBJ);
+            $this->count = $this->query->rowCount();
         }
-        $this->results =$this->query->fetchAll(PDO::FETCH_OBJ);
         return $this;
     }
 
@@ -38,4 +57,89 @@ class DB
     {
         return $this->error;
     }
+
+    /**
+     * @return array|object
+     */
+    public function getResults(): array|object
+    {
+        return $this->results;
+    }
+
+    function getCount(): int
+    {
+        return $this->count;
+    }
+
+    public function getForTable(string $table, array $where = []): object|bool
+    {
+        return $this->getForAction('SELECT *', $table, $where);
+    }
+
+    public function deleteForTable(string $table, array $where = []): object|bool
+    {
+        return $this->getForAction('DELETE', $table, $where);
+    }
+
+    public function getForAction(string $action, string $table, array $where = []):object|bool
+    {
+        $operators = ['=', '<', '>', '<=', '>='];
+
+        if (count($where) === 3) {
+            $field = $where[0];
+            $operator = $where[1];
+            $value = $where[2];
+
+            if (in_array($operator, $operators)) {
+                $sql = "{$action} FROM {$table} WHERE {$field} {$operator} ?";
+                if (!$this->query($sql, [$value])->getError()) {
+                    return $this;
+                }
+            }
+        }
+        return false;
+    }
+
+    public function insert(string $table, $fields = []): bool
+    {
+        $values = '';
+        foreach ($fields as $field) {
+            $values .= '?,';
+        }
+        $values =  rtrim($values, ',');
+
+        $sql = "INSERT INTO {$table} (`"  . implode('`, `', array_keys($fields)) . "`) VALUES (" . $values . ")";
+
+        if(!$this->query($sql, $fields)->getError()){
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * @param $table
+     * @param $id
+     * @param $fields
+     * @return bool
+     */
+    public function update($table, $id, $fields = []):bool
+    {
+        $set ='';
+        foreach ($fields as $key => $field) {
+            $set .= "{$key} = ?, ";
+        }
+        $set = rtrim($set, ', ');
+        $sql = "UPDATE {$table} SET {$set} WHERE `id` = {$id}";
+
+        if (!$this->query($sql, $fields)->getError()) {
+            return true;
+        }
+        return false;
+    }
+
+    public function first()
+    {
+        return $this->results[0];
+    }
+
 }
--- index.php
+++ index.php
@@ -1,13 +1,30 @@
 <?php
 require_once 'classes/DB.php';
 
-$posts = DB::getInstanse()->query('SELECT * FROM posts');
-if ($posts->getError()) {
-    echo 'we have error';
-}else{
-    echo 'ok';
-}
-die();
-echo '<pre>';
-var_dump($posts);
-echo '</pre>';
+//$users = DB::getInstanse()->query('SELECT * FROM users WHERE name  = ?', ['John Doe']);
+
+$users = DB::getInstanse()->getForTable('users', ['username', '=', 'PaveL']);
+
+//DB::getInstanse()->deleteForTable('users', ['username', '=', 'test']);
+//$users = DB::getInstanse()->insert('users', [
+//    'username' => 'PaveL',
+//    'password_hash' => 'password'
+//]);
+//DB::getInstanse()->update('users', 7, [
+//    'username'=>'user',
+//    'password_hash'=>'123'
+//]);
+//if ($users->getError()) {
+//    echo 'we have error';
+//} else {
+//    foreach ($users->getResults() as $result) {
+//        echo $result->id;
+//        echo '<br>';
+//        echo $result->username;
+//        echo '<br>';
+//    }
+//}
+//echo '<hr>';
+//echo 'Количество записей:' . $users->getCount();
+
+var_dump($users->getResults());
